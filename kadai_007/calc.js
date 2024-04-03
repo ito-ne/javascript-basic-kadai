380c3530321a320995decd857160949ca70d54d7
ito-ne <160625476+ito-ne@users.noreply.github.com>
2024-04-03 18:55:05 +0900

    calc.jsを修正しました

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -7,7 +7,7 @@ num = Math.floor(Math.random()*20);
 // 変数numを確認のため出力
 console.log(num);
 
-// 条件分岐_3の倍数、5の倍数、3と5の倍数、それ以外
+// 条件分岐_3と5の倍数、3の倍数、5の倍数、それ以外
 if (num % 3 === 0 && num % 5 === 0){
   console.log('3と5の倍数です');
 }
