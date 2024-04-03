5394c17e28464aac05903b0eed2ab2960499cb25
ito-ne <160625476+ito-ne@users.noreply.github.com>
2024-04-03 18:50:26 +0900

    calc.jswo

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -8,16 +8,15 @@ num = Math.floor(Math.random()*20);
 console.log(num);
 
 // 条件分岐_3の倍数、5の倍数、3と5の倍数、それ以外
-if(num % 3 === 0){
+if (num % 3 === 0 && num % 5 === 0){
+  console.log('3と5の倍数です');
+}
+else if(num % 3 === 0){
   console.log('3の倍数です');
 }
 
 else if (num % 5 === 0){
   console.log('5の倍数です');
-}
-
-else if (num % 3 === 0 && num % 5 === 0){
-  console.log('3と5の倍数です');
 }
 
 else {
